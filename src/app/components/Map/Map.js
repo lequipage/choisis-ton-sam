b6552041efe565feee32c885d4513de87b2f2127
HeadFox <lucien.headfox@gmail.com>
2017-12-08 01:42:54 +0100

    chore: add Map component base #10

---
+++ src/app/components/Map/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import Header from './Header';
+// import Box from '../../components/Box';
+// import Login from '../../components/Login';
+// import './Map.scss';
+
+const Map = () => (
+  <div className="create">
+    Google Map
+  </div>
+);
+
+export default Map;
