f02d0f1bd936c0e14b4fb2e6b5f2f764148e0a19
HeadFox <lucien.headfox@gmail.com>
2017-12-08 01:43:35 +0100

    chore: add action page component page 2

---
+++ src/app/pages/Actions/Actions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Box from '../../components/Box';
+import Create from '../Create';
+// import './Home.scss';
+
+const Actions = () => (
+  <div id="home" className="home">
+    <Header />
+    <Box type="create" />
+    <Box type="current" />
+    <Box type="past" />
+    <Create />
+  </div>
+);
+
+export default Actions;
---
+++ src/app/pages/Actions/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import './Home.scss';
+
+const Home = () => (
+  <header className="header">
+    <h1>Bienvenue</h1>
+  </header>
+);
+
+export default Home;
